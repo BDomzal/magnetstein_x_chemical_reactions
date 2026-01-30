9d3b737678925cd4a579c6fd95acf5655e73449c
BDomzal <basiadomzal@gmail.com>
2026-01-30 20:49:45 +0100

    Cleaned code: additional analysis incomplete library

---
+++ notebooks/additional_analysis/additional_analysis_incomplete_library.py
@@ -0,0 +1,68 @@
+import json
+
+with open('../../config.json', 'r') as f:
+    config = json.load(f)
+
+MAGNETSTEIN_PATH = config["magnetstein_path"]
+SRC_PATH = config["src_path"]
+
+import sys
+sys.path.insert(0, MAGNETSTEIN_PATH)
+sys.path.insert(1, SRC_PATH)
+from utils import *
+
+
+experiment_name = 'Saccharose hydrolysis'
+
+MIXTURE_PATH = config["mixture_paths"][experiment_name]
+SUCROSE_TIME_AXIS_PATH = config["sucrose_time_axis_path"]
+MIXTURE_SEPARATOR = config["mixture_separators"][experiment_name]
+REAGENTS_PATH = config["reagents_paths"][experiment_name]
+REAGENTS_SEPARATORS = config["reagents_separators"][experiment_name]
+SATURATED_COLORS_FOR_COMPONENTS = config["saturated_colors_for_components"][experiment_name]
+SUBSTANCES_NAMES = config["substances_names_for_visualisations"][experiment_name]
+ADDITIONAL_ANALYSIS_RESULTS_PATHS = config["additional_analysis_results_paths_incomplete_library"][experiment_name]
+
+
+time_range = get_time_range(experiment_name, '../' + SUCROSE_TIME_AXIS_PATH)
+
+
+mixture_time_data = load_mixture_time_data_v3(experiment_name, '../' + MIXTURE_PATH, MIXTURE_SEPARATOR)
+
+
+reagents_spectra = load_and_preprocess_reagents_spectra(['../' + reag_path for reag_path in REAGENTS_PATH], REAGENTS_SEPARATORS)
+
+kappa = 0.1
+kappa_th = 1
+
+
+for nr in [0, 1, 2, 3, None]:
+
+    which_reagent_to_remove = nr
+
+
+    reagents_spectra = load_and_preprocess_reagents_spectra(['../' + reag_path for reag_path in REAGENTS_PATH], REAGENTS_SEPARATORS)
+    reagents_spectra = reduce_library(reagents_spectra, which_reagent_to_remove)
+
+
+    proportions_in_times = []
+    noise_proportions_in_times = []
+    noise = []
+    noise_in_components = []
+
+    estimation = estimate_proportions_in_time(np.array(mixture_time_data), reagents_spectra, 
+                                            what_to_compare='area', 
+                                            solver=pulp.GUROBI(msg=False),
+                                            MTD=kappa, MTD_th=kappa_th,
+                                             verbose=True)
+
+    proportions_in_times = estimation['proportions_in_time']
+    noise_proportions_in_times = estimation['proportion_of_noise_in_reagents_in_time']
+    noise = estimation['noise_in_mixture_in_time']
+    noise_in_components = estimation['noise_in_reagents_in_time']
+    common_horizontal_axis = estimation['common_horizontal_axis_in_time']
+
+
+    create_incomplete_library_figure(proportions_in_times, time_range, SUBSTANCES_NAMES, which_reagent_to_remove,
+                                        SATURATED_COLORS_FOR_COMPONENTS, kappa, kappa_th, results_path=ADDITIONAL_ANALYSIS_RESULTS_PATHS)
+
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from masserstein import NMRSpectrum, estimate_proportions, estimate_proportions_in_time
+import pulp
+import time
+from textwrap import wrap
 
 def load_spectrum(mixture_time_data, moment_of_time):
     ppm = mixture_time_data['ppm']
@@ -94,3 +97,140 @@ def preprocess_mnova_integrals(integrals_path, raw_integrals_separators, substan
     integrals.columns = substances_names
     integrals[integrals < 0] = 0
     return integrals
+
+def get_time_range(experiment_name, sucrose_time_axis_path):
+
+    if experiment_name.startswith('Saccharose hydrolysis'):
+        return (1/60)*np.loadtxt(sucrose_time_axis_path)[::10]
+    elif experiment_name.startswith('PMG 284 monitoring'):
+        return [(1/60)*5*(el-1) for el in list(range(1,1000,10))]
+    elif experiment_name.startswith('PMG 287 monitoring'):
+        return [(1/60)*5*(el-1) for el in list(range(1,1000,10))]
+    else:
+        return
+
+def get_time_range_integrals_python(experiment_name, sucrose_time_axis_path):
+
+    if experiment_name.startswith('Saccharose hydrolysis'):
+        return (1/60)*np.loadtxt(sucrose_time_axis_path)
+    elif experiment_name.startswith('PMG 284 monitoring'):
+        return [(1/60)*5*(el-1) for el in list(range(1,1000))]
+    elif experiment_name.startswith('PMG 287 monitoring'):
+        return [(1/60)*5*(el-1) for el in list(range(1,1000))]
+    else:
+        return
+
+def get_time_range_integrals_mnova(experiment_name, sucrose_time_axis_path):
+    if experiment_name.startswith('Saccharose hydrolysis'):
+        return (1/60)*np.loadtxt(sucrose_time_axis_path)[:-1]
+    elif experiment_name.startswith('PMG 284 monitoring'):
+        return [(1/60)*5*(el-1) for el in list(range(1,1001))]
+    elif experiment_name.startswith('PMG 287 monitoring'):
+        return [(1/60)*5*(el-1) for el in list(range(1,1001))]
+    else:
+        return
+
+def load_mixture_time_data_v3(experiment_name, mixture_path, mixture_separator):
+
+    mixture_time_data = pd.read_csv(mixture_path, sep = mixture_separator)
+
+    if experiment_name.startswith('Saccharose hydrolysis'):
+        ppm = mixture_time_data.iloc[:,0:1]
+        every_10th = mixture_time_data.iloc[:,1:].iloc[:,::10]
+        mixture_time_data = pd.concat((ppm, every_10th), axis=1)
+
+    elif experiment_name.startswith('PMG 284 monitoring'):
+        ppm = mixture_time_data.iloc[:,:-1].iloc[:,0:1]
+        every_10th = mixture_time_data.iloc[:,:-1].iloc[:,1:].iloc[:,::10]
+        mixture_time_data = pd.concat((ppm, every_10th), axis=1)
+
+    elif experiment_name.startswith('PMG 287 monitoring'):
+        ppm = mixture_time_data.iloc[:,:-1].iloc[:,0:1]
+        every_10th = mixture_time_data.iloc[:,:-1].iloc[:,1:].iloc[:,::10]
+        mixture_time_data = pd.concat((ppm, every_10th), axis=1)
+
+    names = ['ppm'] + ['t' + str(nb) for nb in range(1, mixture_time_data.shape[1])]
+    mixture_time_data.columns = names
+    return mixture_time_data
+
+def load_and_preprocess_reagents_spectra(reagents_path, reagents_separator):
+
+    reagents_spectra = []
+    for reagent in reagents_path:
+        reag = pd.read_csv(reagent, sep=reagents_separator, header=None).iloc[:,:2]
+        reagents_spectra.append(reag)
+
+    reagents_spectra2 = []
+    for reag in reagents_spectra:
+        ppm = reag.iloc[:,0]
+        ints = reag.iloc[:,1]
+        sp = NMRSpectrum(confs = list(zip(ppm, ints)))
+        reagents_spectra2.append(sp)
+
+    reagents_spectra = reagents_spectra2
+
+    for sp in reagents_spectra:
+        sp.trim_negative_intensities()
+        sp.normalize()
+
+    return reagents_spectra
+
+def reduce_library(reagents_spectra, which_reagent_to_remove):
+    if which_reagent_to_remove is None:
+        return reagents_spectra
+    else:
+        res = reagents_spectra[:which_reagent_to_remove] + reagents_spectra[(which_reagent_to_remove + 1):]
+        return res
+
+def get_names_and_colors(substances_names, saturated_colors_for_components, which_reagent_to_remove=None):
+
+    color_list = saturated_colors_for_components
+
+    if which_reagent_to_remove is None:
+        return substances_names, color_list
+    else:
+        color_list = color_list[:which_reagent_to_remove] + color_list[(which_reagent_to_remove + 1):]
+        names = substances_names[:which_reagent_to_remove] + substances_names[(which_reagent_to_remove + 1):]
+
+    return names, color_list
+
+def create_incomplete_library_figure(proportions_in_times, time_range, substances_names, which_reagent_to_remove,
+                                    saturated_colors_for_components, kappa, kappa_th, results_path=None):
+
+
+    names, color_list= get_names_and_colors(
+                                           substances_names,
+                                           saturated_colors_for_components,
+                                           which_reagent_to_remove
+                                            )
+    res = np.array(proportions_in_times)
+    _, nr_of_reagents = res.shape
+
+    for reag_nr in range(nr_of_reagents):
+        plt.plot(time_range, res[:,reag_nr], '.', color=color_list[reag_nr],
+                label = names[reag_nr])
+
+    if which_reagent_to_remove is None:
+        plt.title('Magnetstein with complete library \n', size=20)
+    else:
+        plt.title('\n'.join(wrap('Magnetstein with incomplete library: missing ' + substances_names[which_reagent_to_remove],
+                                40)
+                           ),
+                  size=20)
+        plt.plot(time_range, 1-res.sum(axis=1), '.', color='#5e5e5e', label='removed signal')
+        
+    plt.xlabel('Time [min]')
+    plt.ylabel('Proportion')
+    leg = plt.legend(fontsize=15)
+    for handle in leg.legend_handles:
+        handle.set_markersize(15)
+
+    plt.tight_layout()
+
+    if results_path is None:
+        plt.show()
+    else:
+        if which_reagent_to_remove is None:
+            plt.savefig(results_path + 'all_components_kappa_'+str(kappa)+'_'+str(kappa_th)+'.png')
+        else:
+            plt.savefig(results_path + 'removed_component_'+str(which_reagent_to_remove)+'_kappa_'+str(kappa)+'_'+str(kappa_th)+'.png')
